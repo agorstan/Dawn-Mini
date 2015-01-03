a4d3a23b4d15ea6d266769a05a9038c4c3446ca7
Alex Gorstan <agorstan@gmail.com>
2015-01-03 14:32:33 -0500

    First working version
    Single color palette always on and cycling.

--- dawn_mini.ino
+++ dawn_mini.ino
@@ -7,45 +7,61 @@
 // 
 
 #define NUM_LEDS 4       // How many leds are in the strip?
-#define DATA_PIN 0       // Data pin that led data will be written out over
+#define LED_PIN 0       // Data pin that led data will be written out over
 #define SWITCH_PIN 2   // the number of the orientation switch pin
-#define LED_PIN 1     // the number of the onboard LED pin
-
-int buttonState = 0;
+#define BRIGHTNESS  255
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+#define UPDATES_PER_SECOND 40
 
 CRGB leds[NUM_LEDS];     // This is an array of leds.  One item for each led in your strip.
 
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
+//extern CRGBPalette16 myRedWhiteBluePalette;
+//extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
 
 void setup() {
-   // initialize the LED pin as an output:
-    pinMode(LED_PIN, OUTPUT);      
-    // initialize the pushbutton pin as an input:
-    pinMode(SWITCH_PIN, INPUT);     
-    // initialize the LED strip
-    FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+    
+    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.setBrightness(  BRIGHTNESS );
+    
+    SetupPurpleAndGreenPalette();
+    currentBlending = BLEND;
+    
 }
 
-// This function runs over and over, and is where you do the magic to light
-// your leds.
 void loop() {
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 1; /* motion speed */
+
+  FillLEDsFromPaletteColors( startIndex);
+
+  FastLED.show();
+  FastLED.delay(1000 / UPDATES_PER_SECOND);
+}
+
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+  uint8_t brightness = 255;
   
-  // Move a single white led 
-  for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
-    // Turn our current led on to white, then show the leds
-    leds[whiteLed] = CRGB::White;
-
-    // Show the leds (only one of which is set to white, from above)
-    FastLED.setBrightness(isOn() ? 255 : 0 );
-    FastLED.show();
-
-    // Wait a little bit
-    delay(100);
-
-    // Turn our current led back to black for the next loop around
-    leds[whiteLed] = CRGB::Black;
+  for( int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+    colorIndex += 90;
   }
 }
 
-bool isOn(){
-  return (digitalRead(SWITCH_PIN)==HIGH) ? 1 : 0;
+void SetupPurpleAndGreenPalette()
+{
+  CRGB purple = CHSV( HUE_PURPLE, 255, 255);
+  CRGB green  = CHSV( HUE_GREEN, 255, 255);
+  CRGB black  = CRGB::Black;
+  
+  currentPalette = CRGBPalette16(CRGB::White,  CRGB::Cyan,  CRGB::White,  CRGB::Purple, CRGB::Green);
+    
+    //,
+    //CRGB::White, CRGB::Cyan, CRGB::White,  CRGB::Green,
+    //CRGB::Cyan,  CRGB::White,  CRGB::Purple,  CRGB::White,
+    //CRGB::Green, CRGB::White, CRGB::White, CRGB::Pink
 }
